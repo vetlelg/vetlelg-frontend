1960b94a55dd1005a455cbfbb4d3e111acb464ed
vetlelg <61113128+vetlelg@users.noreply.github.com>
2024-09-16 05:50:56 +0200

    fixing

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,11 +2,12 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 const Profile = () => (
-    <div className="pt-lg-5">
-            <h1 className="fw-bold display-5">Vetle L. Gundersen</h1>
+    <div className="row pt-lg-5">
+        <div className="col-sm-8 col-md-6 col-lg-12">
+            <h1 className="fw-bold display-5 text-nowrap">Vetle L. Gundersen</h1>
             <h5 className="fw-medium">Software Engineering Student</h5>
             <p style={{width: 300}}>Looking for new opportunities to gain more experience and learn new things.</p>
-            <Navbar className="pb-4">
+            <Navbar className="pb-5">
             <Nav>
                 <Nav.Link className="pt-0 ps-0 pe-3" href="https://linkedin.com/in/vetlelg">LinkedIn</Nav.Link>
                 <Nav.Link className="pt-0 pe-3" href="https://github.com/vetlelg">GitHub</Nav.Link>
@@ -14,7 +15,8 @@ const Profile = () => (
                 <Nav.Link className="pt-0" href="#">Resume</Nav.Link>
             </Nav>
             </Navbar>
-            <img className="col-8 col-sm-6 col-md-5 col-lg-9 rounded-circle" src={require("../assets/images/profile.JPEG")} alt="Vetle L. Gundersen" />
+        </div>
+        <img className="col-9 col-sm-7 col-md-6 col-lg-9 rounded-circle" src={require("../assets/images/profile.JPEG")} alt="Vetle L. Gundersen" />
     </div>
 );
 
