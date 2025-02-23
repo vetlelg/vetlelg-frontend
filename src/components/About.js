f83c96a818817ea371cd5b77f07235508ec1bcdf
vetlelg <61113128+vetlelg@users.noreply.github.com>
2025-02-23 17:56:09 +0100

    updated bio

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ const About = () => (
     <div id="about" className="row pt-5">
         <h6 className="fw-bold">ABOUT</h6>
         <p className="pt-4">
-            I'm a Software Engineering student with a background as an IT Technician, set to complete my Bachelor's degree in Spring 2025.
+            I'm a Software Engineering student with a background as an IT Technician, set to complete my Bachelor's degree in Spring 2025. I love tackling complex challenges and continuously expanding my skill set. I am actively seeking opportunities to contribute and grow professionally.
         </p>
         <p>
             Through my studies and hands-on projects, I've gained experience with a range of technologies, including:
@@ -12,10 +12,6 @@ const About = () => (
             <br/>Frontend: JavaScript/React, Bootstrap, CSS/HTML
             <br/>DevOps &amp; Databases: SQL, Docker, Azure, and more
         </p>
-        <p>
-            I love tackling complex challenges and continuously expanding my skill set. I am actively seeking opportunities to contribute and grow professionally.
-        </p>
-
         <img className="mt-5 d-md-none col-10 col-sm-8 mx-auto rounded-circle" src="/profile.JPEG" alt="Vetle L. Gundersen" />
     </div>
 );
